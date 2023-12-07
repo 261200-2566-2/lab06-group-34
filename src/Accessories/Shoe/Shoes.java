29be6c7d568004b29b55b3ecdd35fcbb00644cbf
Natan <Th3than@gmail.com>
2023-12-07 21:31:38 +0700

    Commend Shoe and Ring

--- src/Accessories/Shoe/Shoes.java
+++ src/Accessories/Shoe/Shoes.java
@@ -6,13 +6,28 @@ public class Shoes {
     public double Shoesspeed;
     protected double baseATK;
     protected double Shoeatk;
+    /**
+     * set Shoe's speed
+     * @param basespeed  to set Shoe's speed
+     * effect : set Shoe's speed to this basespeed and Shoe's basespeed to this basespeed
+     */
     public Shoes(double basespeed){
         this.basespeed = basespeed;
         Shoesspeed = basespeed;
     }
+    /**
+     * Get Shoe's speed
+     * @return Shoesspeed is speed of this Shoe
+     */
     public int Speed(){
         return (int)(Shoesspeed);
     }
+    /**
+     * Shoe level UP
+     * effect : Sword level + 1
+     *         recalculate Shoe's speed
+     *         print congratulation for Shoe level up
+     */
     public void lvlUP(){
         lvl++;
         Shoesspeed = basespeed * (1 + 0.05 * lvl);
--- src/Accessories/Shoe/SpeedShoe.java
+++ src/Accessories/Shoe/SpeedShoe.java
@@ -2,6 +2,11 @@ package Accessories.Shoe;
 
 
 public class SpeedShoe extends Shoes {
+    /**
+     * set Shoe's speed
+     * @param speed  to set Shoe's speed
+     * effect : set Shoe's speed to this speed*1.2
+     */
     public SpeedShoe(double speed){
         super(speed*1.2);
     }
